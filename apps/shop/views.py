a23fbe40fe7b20924a537722331c5f4eee05c3c0
rlyost <ry@ryost.us>
2018-03-20 11:43:57 -0700

    finished Amadon

--- apps/shop/views.py
+++ apps/shop/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render, redirect
-from time import gmtime, strftime
 
 def index(request):
     if 'shop' not in request.session:
@@ -7,20 +6,22 @@ def index(request):
         item2 = { 'item': 'Dojo Sweater', 'price': 29.99, 'product_id': 1002 }
         item3 = { 'item': 'Dojo Cup', 'price': 4.99, 'product_id': 1003 }
         item4 = { 'item': 'Algorithm Book', 'price': 49.99, 'product_id': 1004 }
+        request.session['life_dollars'] = 0
+        request.session['life_items'] = 0
         request.session['shop'] = [item1, item2, item3, item4]
-    return render(request, 'shop/index.html', all_items=request.session['shop'])
+    return render(request, 'shop/index.html')
 
 def buy(request):
     if request.method == "POST":
-        product_id = request.POST['product_id']
-        quantity = request.POST['quantity']
+        request.session['cart'] = 0
+        product_id = int(request.POST['product_id'])
+        quantity = int(request.POST['quantity'])
         for item in request.session['shop']:
-            if item['product_id'] == product_id:
-                cart = quantity * item['price']
+            if int(item['product_id']) == product_id:
+                request.session['cart'] = quantity * item['price']
         request.session['life_dollars'] += request.session['cart']
         request.session['life_items'] += quantity
-        request.session['context'] = [cart, request.session['life_items'], request.session['life_dollars']]
-    return redirect("/checkout", request.session['context'])
+    return redirect("/checkout")
 
 def checkout(request):
-    return render(request, 'shop/checkout.html', request.session['context'])
+    return render(request, 'shop/checkout.html')
